f688ca5851efaf52359eed2b2ee057aac748a74c
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-07 13:18:23 +0530

    PHP

---
+++ 02_First.php
@@ -0,0 +1,11 @@
+<!-- HELLO WORD IN PHP LANGUAGE -->
+
+<!-- 1 Make file with the .php extension
+2 Make php tag
+3 write Code 
+4 Display Out on screen -->
+
+<?php
+echo "Sahil Khola  ";
+echo 100
+?>
