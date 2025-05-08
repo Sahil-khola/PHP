c898b2517af7fb191f5dc882be0efa8cd04ec8d3
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-08 10:11:38 +0530

    PHP

---
+++ 04_Variable.php
@@ -0,0 +1,44 @@
+<?php
+
+$Fname = 'Sahil';
+$Lname = "Khola";
+
+echo $Fname;
+echo $Lname;
+echo '<br>';
+
+echo $Fname ;
+echo $Lname;
+echo '<br>';
+
+echo $Fname ;
+echo $Lname;
+echo '<br>';
+
+echo $Fname ;
+echo $Lname;
+echo '<br>';
+
+$num = 5;
+
+echo $num*2;
+echo "<br>";
+
+echo $num*5;
+echo "<br>";
+
+echo $num*8;
+echo "<br>";
+
+echo $num*9;
+echo "<br>";
+
+echo $num*10;
+echo "<br>";
+
+
+$any = 'Sahil';
+echo $any;
+
+
+?>
