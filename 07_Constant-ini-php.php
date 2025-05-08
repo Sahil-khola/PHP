a661ec5a320da5760e1d63c4449e82d64ae4689d
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-08 23:16:42 +0530

    PHP

---
+++ 07_Constant-ini-php.php
@@ -0,0 +1,20 @@
+        <?php
+
+// variable :-------->
+// $name = 'sahil khola';
+// $name = 'Rao shabh';
+// echo $name;
+
+
+// constant-->
+
+// const name = 'Sahil khola';
+// const name1 = 'Rao shabh';
+// echo name, name1;
+
+define('name', 'sahil');
+// define('name', 'khola');
+echo name;
+
+
+?>
---
+++ 08_DataType.php
@@ -0,0 +1,58 @@
+ <!-- <?php
+echo 'ALL types of Data type in PHP <br>';
+echo 'int <br>';
+echo 'float <br>';
+echo 'string <br>';
+echo 'boolean <br>';
+echo 'null <br>';
+echo 'array <br>';
+echo 'object <br>';
+echo 'recource <br>';
+?>  -->
+
+
+
+<?php
+
+$Name = "Sahil";//String
+$Age = 20; // int
+$_12th_Class = 80.3; // Float
+$is_12th_Pass = true; // false ; //Boolean
+$Arr = ['Sahil',33.3,false,500]; // Array
+$Empty = null;
+
+
+
+// echo $Name;
+// echo var_dump($Name);
+
+
+// echo $Age;
+// echo var_dump($Age);
+
+
+// echo $_12th_Class;
+// echo var_dump($_12th_Class)
+
+
+// echo $is_12th_Pass;
+// echo var_dump(($is_12th_Pass))
+
+
+// echo $Arr[2];
+// echo var_dump($Arr);
+
+// echo $Empty;
+// echo var_dump($Empty);
+
+
+
+
+
+
+
+
+
+
+
+?>
