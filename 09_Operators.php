60c8349cfde6a06772981e7a743d3c8584b33a06
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-09 13:59:24 +0530

    PHP

---
+++ 09_Operators.php
@@ -0,0 +1,12 @@
+<?php
+$a = 10;
+$b = 10;
+$c = ($a==$b);
+echo var_dump($c);
+
+echo 2+2 // 2+2= 4  where 2 is oprends and + is operator or 4 is output
+
+
+
+
+?>
---
+++ 10_Arithmetic_operators.php
@@ -0,0 +1,12 @@
+<?php
+$a = 10;
+$b = 5;
+
+// echo $a + $b;   Additional
+// echo $a - $b;   substract
+// echo $a * $b;   multiple
+// echo $a / $b;   divison
+// echo $a % $b;   modolus
+// echo $a ** $b;  exponentation
+
+?>
