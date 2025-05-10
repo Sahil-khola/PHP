5b33a5f44964b65300b11769a4645dff61601e67
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-10 22:48:01 +0530

    PHP

---
+++ 13_Increment_decrement.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 10;
+
+// echo ++$a; // pre increment return $a by one ,then return update value
+// echo $a++; // post increment return $a after add one by one
+
+// echo --$a; // pre decrement 
+// echo $a--; // pre decrement
+// echo $a;
+?>
---
+++ 14_String.php
@@ -0,0 +1,19 @@
+<!-- What is string?
+String is a data type which is used to store text and is written between double quotes or single quotes. -->
+
+<?php
+
+$name = 'Sahil khola';
+$str1 = "Rao shabh";
+$str2 = '10';
+
+echo var_dump($name);
+echo '<br>';
+
+echo var_dump($str1);
+echo '<br>';
+
+echo var_dump($str2);
+echo '<br>';
+
+?>
