baf8b3bf33503c36d45579ccea0d30b646775db7
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-12 13:41:56 +0530

    PHP

---
+++ 19_Switch_Statement.php
@@ -0,0 +1,29 @@
+<?php
+
+$color = 'black';
+
+switch($color){
+    case  'red';
+    echo 'Red color is selected';
+    break;
+    case  'black';
+    echo 'black color is selected';
+    break;
+    case  'white';
+    echo 'white color is selected';
+    break;
+    case  'blue';
+    echo 'blue color is selected';
+    break;
+    case  'orange';
+    echo 'Red color is selected';
+    break;
+    case  'brown';
+    echo 'brown color is selected';
+    break;
+    case  'green';
+    echo 'green color is selected';
+    break;
+}
+
+?>
