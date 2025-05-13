1e237a5b3506cb9b8dc42a5b7f9601ecffd52349
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-13 23:11:49 +0530

    PHP

--- 26_DO_while_loop.php
+++ 26_DO_while_loop.php
@@ -1,2 +1,12 @@
 <!-- what is do-while loop?
 A do-while loop is a loop that executes a block of code once, and then checks a condition. If the condition is true, the code is executed again. -->
+
+<?php
+
+$num = 1;
+do {
+  echo "$num <br>";
+  $num++;
+} while ($num<=10);
+
+?>
