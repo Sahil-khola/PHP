758ac2b9e5966d79b3605292932d24ae6b8a8f13
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-17 22:56:03 +0530

    PHP

--- 31_Function_with_default-parameter.php
+++ 31_Function_with_default-parameter.php
@@ -1,10 +1,12 @@
 <?php
 
-function colorChnge($Name,$color="yellow"){
+function colorChnge($Name = 'Khola',$color="yellow"){
     echo "<h1 style = 'color:$color'>$Name</h1>";
 }
 
 colorChnge("Sahil",'green');
 colorChnge("Sahil");
+colorChnge("pink");
+
 
 ?>
