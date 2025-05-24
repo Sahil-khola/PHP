40e733806f9831dcded288f76a27b4ad8890dda9
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-24 15:04:45 +0530

    PHP

---
+++ 46_Get_Request_Method.php
@@ -0,0 +1,11 @@
+<?php
+// echo "this is Php Login ";
+if ($_GET) {
+    echo "User Name : ";
+    echo $_GET['name'];
+    echo "<br>";
+
+    echo "Password : ";
+    echo $_GET['password'];
+    echo "<br>";
+}
