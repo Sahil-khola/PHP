3ea44ffe3dfcf96e8cd4cafa3dab811fbea09434
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-24 12:02:26 +0530

    PHP

---
+++ 41_Array_Function.php
@@ -0,0 +1,13 @@
+<?php
+
+$user = ["Sahil ","khola ","Rao Shabh "];
+
+array_push($user,"Neeraj","Rinku","Tinku");
+// array_pop($user);
+// print_r($user);
+// echo "<br>";
+// array_splice($user,-2);
+// print_r($user);
+
+
+?>
