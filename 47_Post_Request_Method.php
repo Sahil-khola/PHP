25286149ee637fb18e2371c8f75396d3caf6a2eb
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-24 23:44:54 +0530

    PHP

---
+++ 47_Post_Request_Method.php
@@ -0,0 +1,10 @@
+<?php
+if ($_POST) {
+    echo "the User Name is ". $_POST["user_name"];
+    echo "<br>";
+    echo "the User email is ". $_POST["user_email"];
+    echo "<br>";
+    echo "the User password is ". $_POST["user_password"];
+    echo "<br>";
+}
+?>
