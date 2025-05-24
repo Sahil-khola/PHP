e49702a2cb5b32e891e76dc1073a6f6d00577254
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-24 14:41:13 +0530

    PHP

---
+++ 45_Html_interact_with_php.php
@@ -0,0 +1,18 @@
+<!-- What does this mean?
+Ans : You can use HTML elements inside PHP code, and PHP code inside HTML elements
+
+request types?
+GET
+POST
+delete 
+Put/Patch
+
+what is get request?
+Ans : A GET request is a type of HTTP request that is used to retrieve data from a server.
+      it request is Visible in url bar of browser
+      less secure than post request
+
+what is post request?
+Ans : A POST request is a type of HTTP request that is used to send data to a server.
+      it request is not Visible in url bar of browser
+      more secure than get request -->
