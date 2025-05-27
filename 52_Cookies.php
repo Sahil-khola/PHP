2c9c4bf885170658fb571843f9beb406647a275e
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-27 22:16:32 +0530

    PHP

--- 52_Cookies.php
+++ 52_Cookies.php
@@ -1,5 +1,6 @@
-What is Cookies?
+<!-- What is Cookies?
 Cookies are small text files that are sent from a web server to a web browser and stored on the user's computer.
 
-Cookies are used to store information about a user's preferences, such as their language, time zone, and other settings.
+Cookies are used to store information about a user's preferences, such as their language, time zone, and other settings. -->
+
 
