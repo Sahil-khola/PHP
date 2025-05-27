70fd1bfd3e1eb3c0d0c7fe79d9881d9b38e696f4
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-27 23:03:39 +0530

    PHP

--- 52_Cookies.php
+++ 52_Cookies.php
@@ -4,3 +4,26 @@ Cookies are small text files that are sent from a web server to a web browser an
 Cookies are used to store information about a user's preferences, such as their language, time zone, and other settings. -->
 
 
+<!-- syntax of cookies
+setcookie(name, value, expire, path, domain, secure, httponly);
+ -->
+
+ <?php
+ 
+ setcookie("Name","Sahil khola",time()+(90000),'/');
+ setcookie("color","Red",time()+(90000),'/');
+
+ if (isset($_COOKIE['Name'])) {
+    print_r($_COOKIE['Name']);
+ } else {
+    echo "No Cookies are found";
+ }
+ echo "<br>";
+ if (isset($_COOKIE['color'])) {
+    print_r($_COOKIE['color']);
+ } else {
+    echo "No color  are found";
+ }
+ 
+ 
+ ?>
