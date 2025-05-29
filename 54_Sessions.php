3edb126e6d19ed66de22f035817fe1581d892929
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-29 16:01:43 +0530

    PHP

---
+++ 54_Sessions.php
@@ -0,0 +1,9 @@
+<!-- What is Sessions?
+A session is a way to store data in the server to be used across multiple pages.
+ -->
+
+<?php
+//  session_start();
+//  $_SESSION['Name']="Sahil khola";
+//  echo "Session will be set"
+?>
