d01723f3b0abdaafe88b1f3e5c64a4da5b13aee3
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-05-29 23:17:13 +0530

    PHP

---
+++ 56_Create_file.php
@@ -0,0 +1,19 @@
+<?php
+// if (isset($_POST['Filename'])) {
+    // $filename = "upload/".$_POST['Filename'];
+    // $content = $_POST['content'];
+
+    // $file = fopen($filename,"w");
+    // fwrite($file,$content);
+    // fclose($file);}
+?>
+<!-- <form action="" method="post">
+    <input type="text" name="Filename" placeholder="enter file name">
+    <br><br>
+    <textarea name="content" >
+
+    </textarea>
+    <br><br>
+    <button>Create file</button>
+</form> -->
+
