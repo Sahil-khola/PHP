1277feeb77fce4a42e23b924d29cc9ecf13ef102
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-06-02 12:26:25 +0530

    PHP

---
+++ 62_Array_function.php
@@ -0,0 +1,11 @@
+<?php
+$users = ["Sahil",'Sourav',"Tinku","Rinku"];
+
+// echo is_array($users);
+// echo count($users);
+// unset($users[1]);
+// array_push($users,"Rao shabh");
+array_pop($users);
+print_r($users);
+
+?>
