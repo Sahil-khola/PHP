293851daf3b1abfa18e00373810670e57fdf4b68
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-06-17 23:13:59 +0530

    PHP

---
+++ OOPS/05_Inheriance.php
@@ -0,0 +1,45 @@
+<!-- 
+Q: What is Inheritance?
+Ans : Inheritance is a mechanism in object-oriented programming that allows one class to inherit properties and methods from another class.
+-->
+
+<?php
+
+class userAuth
+{
+    function logIn($userType)
+    {
+        echo "$userType login..";
+    }
+}
+
+class student extends userAuth
+{
+
+    function getName()
+    {
+        echo "Sahil khola";
+    }
+}
+
+class teacher extends userAuth 
+{
+    function skills()
+    {
+        echo "DSA PYTHON PHP";
+    }
+}
+
+$s1 = new student();
+$s1->logIn("student");
+echo "<br>";
+$s1->getName();
+echo "<br>";
+$s2 = new teacher();
+$s2->logIn("teacher");
+echo "<br>";
+$s2->skills();
+
+
+
+?>
