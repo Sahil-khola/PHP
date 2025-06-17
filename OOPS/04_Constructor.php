c7c54af555c2c35da02bc336dc20d9b77f459b45
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-06-17 22:34:12 +0530

    PHP

---
+++ OOPS/04_Constructor.php
@@ -0,0 +1,25 @@
+<!-- 
+Q.What is Constructor?
+Ans : A constructor is a special method that is automatically called when an object of a class is created. It is used to initialize the properties of the object.
+ 
+-->
+
+<?php
+
+class Constrctor {
+      
+    public $name;
+    function __construct($name){
+        $this->name = $name;
+    } 
+
+    function display(){
+       echo $this->name;
+    }
+
+}
+
+$c1 = new Constrctor("Rao shabh");
+$c1->display();
+
+?>
