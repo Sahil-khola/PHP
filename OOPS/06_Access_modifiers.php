c22cdf4fcaac25425df52b6255138f807e4fdb71
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-06-18 12:24:37 +0530

    PHP

---
+++ OOPS/06_Access_modifiers.php
@@ -0,0 +1,38 @@
+<!-- What is Access Modifiers -->
+<!-- Access Modifiers in PHP are used to control the visibility of class members -->
+<?php
+
+class Teacher
+{
+
+    public  function QuestionPaper()
+    {
+        echo "Student QuestionPaper";
+    }
+
+    private function allMarks()
+    {
+        echo "All Marks";
+    }
+
+    function marksReview()
+    {
+        $this->allMarks();
+    }
+
+    protected function Marks(){
+          echo "Private marks";
+    }
+}
+class Management extends Teacher{
+    function hiddenMarks(){
+        $this->Marks();
+    }
+}
+$t1 = new Teacher;
+// $t1->QuestionPaper();
+// $t1->marksReview();
+
+$m1 = new Management;
+$m1->hiddenMarks();
+?>
