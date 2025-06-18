125480afaaa5f812bc42cdd288374fff6da3ff9a
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-06-18 23:26:08 +0530

    PHP

--- OOPS/11_Late_Static_blind.php
+++ OOPS/11_Late_Static_blind.php
@@ -1,3 +1,38 @@
 <?php
 
+class countrySale
+{
+
+    static public $totalsale = 1000;
+
+    function getTotalSale()
+    {
+        self::$totalsale;
+    }
+
+    static function areaName()
+    {
+        echo "india";
+    }
+
+    function getareaName(){
+        static::areaName();
+    }
+}
+
+
+
+class citySale extends countrySale
+{
+
+    static public $totalsale = 50;
+    static function areaName()
+    {
+        echo "pakistan";
+    }
+}
+
+$city = new citySale();
+$city->getareaName();
 ?>
+
